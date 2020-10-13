de571334ae708c2f6f29cc8fa7383d513b649c43
bhagawath.m@soprasteria.com <bhagawath.m@soprasteria.com>
2020-10-13 16:28:17 +0530

    new

--- src/main/java/com/soprasteria/devops/controller/CommonController.java
+++ src/main/java/com/soprasteria/devops/controller/CommonController.java
@@ -8,7 +8,7 @@ import com.soprasteria.devops.model.BaseObj;
 @RestController
 public class CommonController {
 	
-	@GetMapping(value="/world")
+	@GetMapping(value="/world1")
 	public BaseObj helloWorld() {
 		
 		return new BaseObj("2034", "DEVOPS Accelerator", "Sopra Steria");
